07be2c3fac41fbd405294ea0f3d5773755a473c3
Paul Cordova <paul.cordova@tecsup.edu.pe>
2021-05-15 22:16:51 -0500

    actualizacion final

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var mercadopago = require('mercadopago');
+var{json} = require("body-parser");
 require('dotenv').config();
 var port = process.env.PORT || 3000
 
@@ -9,6 +10,7 @@ mercadopago.configure({
     integrator_id: process.env.INTEGRATOR_ID,
 });
 var app = express();
+app.use(json());
 
 const cliente = {
     name: 'Lalo',
